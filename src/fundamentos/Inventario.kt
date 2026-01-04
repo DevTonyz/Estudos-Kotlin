da507bf73faddb2e3d3881d229ae2f607741cd5b
Antony <2024019979@aluno.osorio.ifrs.edu.br>
2026-01-04 15:43:45 -0300

    Ajuste nome de item Pocao de Vida

--- src/fundamentos/Inventario.kt
+++ src/fundamentos/Inventario.kt
@@ -5,7 +5,7 @@ fun main(){
     println("O heroi achou uma Espada de diamante!")
     inventory.add("Espada de Diamante")
     println("O heroi achou uma Pocao de Vida!\n")
-    inventory.add("Pocao de Vida\n")
+    inventory.add("Pocao de Vida")
     println(inventory)
     println("\nO heroi bebeu a pocao.")
     inventory.remove("Pocao de Vida\n")
