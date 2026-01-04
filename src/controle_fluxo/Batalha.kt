f8c9724783c4f179fc42d663e010cc2e9dc5f6cb
Antony <2024019979@aluno.osorio.ifrs.edu.br>
2026-01-04 15:29:20 -0300

    Loop while com aleatoridade

--- src/controle_fluxo/Batalha.kt
+++ src/controle_fluxo/Batalha.kt
@@ -2,10 +2,9 @@ package controle_fluxo
 
 fun main(){
     var monsterLife = 50
-    var playerDamage = 10
-
     while (monsterLife > 0) {
-        monsterLife -= playerDamage
+        val damage = (5..20).random()
+        monsterLife -= damage
         println("Golpe acertado! Vida do monstro: [$monsterLife]")
     }
     println("O monstro foi derrotado!")
