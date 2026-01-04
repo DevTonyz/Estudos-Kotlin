ab23b6e47f159a229eefa63d2cd3617ca306c5a0
Antony <2024019979@aluno.osorio.ifrs.edu.br>
2026-01-04 15:53:16 -0300

    finaliza exercicio de listas e loops

--- src/fundamentos/Inventario.kt
+++ src/fundamentos/Inventario.kt
@@ -2,12 +2,18 @@ package fundamentos
 
 fun main(){
     val inventory = mutableListOf("pao velho")
+    var cont = 1
     println("O heroi achou uma Espada de diamante!")
     inventory.add("Espada de Diamante")
     println("O heroi achou uma Pocao de Vida!\n")
     inventory.add("Pocao de Vida")
     println(inventory)
     println("\nO heroi bebeu a pocao.")
-    inventory.remove("Pocao de Vida\n")
-    println(inventory)
+    inventory.remove("Pocao de Vida")
+
+    println("\n-- ITENS NO INVENTARIO --")
+    for (item in inventory){
+        println("$cont: $item")
+        cont++
+    }
 }
